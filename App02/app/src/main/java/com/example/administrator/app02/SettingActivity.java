7f9fb149085140c639c056d2100f43ee726ff274
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-08-14 15:00:49 +0900

    APP01 추가2
    APP01 추가2

--- App02/app/src/main/java/com/example/administrator/app02/SettingActivity.java
+++ App02/app/src/main/java/com/example/administrator/app02/SettingActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.app02;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -11,7 +12,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SettingActivity extends AppCompatActivity {
+import java.util.Set;
+
+public class SettingActivity extends AppCompatActivity implements OnClickListener, OnItemSelectedListener {
 
     Spinner spinner01;  // 1번 종류 스피너
     Spinner spinner02;  // 2번 하위 품목 스피너
@@ -22,6 +25,7 @@ public class SettingActivity extends AppCompatActivity {
 
     Button set_btn;  // 저장버튼
     Button exit_btn;  // 나가기, 취소
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,25 +38,25 @@ public class SettingActivity extends AppCompatActivity {
         set_btn = (Button) findViewById(R.id.set_btn);
         exit_btn = (Button) findViewById(R.id.exit_btn);
 
-        abc_inc.setOnClickListener((View.OnClickListener) SettingActivity.this);
-        abc_dec.setOnClickListener((View.OnClickListener) SettingActivity.this);
-        set_btn.setOnClickListener((View.OnClickListener) SettingActivity.this);
-        exit_btn.setOnClickListener((View.OnClickListener) SettingActivity.this);
+        abc_inc.setOnClickListener(this);
+        abc_dec.setOnClickListener(this);
+        set_btn.setOnClickListener(this);
+        exit_btn.setOnClickListener(this);
 
-        final String[] items = {"선택해주세요", "초속효성", "속효성", "중간형", "혼합형", "지속형"};  // 스피너 1번 : 인슐린 종류
-        final String[] items1 = {"선택해주세요", "휴머로그", "노보래피드", "애피드라"};  // 초속효성
-        final String[] items2 = {"선택해주세요", "휴먼인슐린"};  // 속효성
-        final String[] items3 = {"선택해주세요", "노보린", "노보렛", "휴물린", "인슈라타드"};  // 중간형
-        final String[] items4 = {"선택해주세요", "노보믹스", "휴머로그 믹스", "휴물린", "믹스타드"};  // 혼합형
-        final String[] items5 = {"선택해주세요", "란투스", "레버미어"};  // 지속형
+
+        final String[] items = {"초속효성", "속효성", "중간형", "혼합형", "지속형"};  // 스피너 1번 : 인슐린 종류
+        final String[] items1 = {"휴머로그", "노보래피드", "애피드라"};  // 초속효성
+        final String[] items2 = {"휴먼인슐린"};  // 속효성
+        final String[] items3 = {"노보린", "노보렛", "휴물린", "인슈라타드"};  // 중간형
+        final String[] items4 = {"노보믹스", "휴머로그 믹스", "휴물린", "믹스타드"};  // 혼합형
+        final String[] items5 = {"란투스", "레버미어"};  // 지속형
 
         final Spinner spinner01 = (Spinner) findViewById(R.id.spinner01);
-        spinner01.setOnItemSelectedListener((OnItemSelectedListener) this);
+        spinner01.setOnItemSelectedListener(( OnItemSelectedListener) this);
         final Spinner spinner02 = (Spinner) findViewById(R.id.spinner02);
         spinner02.setOnItemSelectedListener((OnItemSelectedListener) this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SettingActivity.this, android.R.layout.simple_spinner_item, items);
-        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
         spinner01.setAdapter(adapter);
         spinner01.setSelection(0);
 
@@ -88,31 +92,38 @@ public class SettingActivity extends AppCompatActivity {
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {}
-            public void onClick(View v) {
-                String abc_str = (String) abc_num.getText();
-                int inabc = (int) Integer.parseInt(abc_str);
-                switch (v.getId()) {
-                    case R.id.set_btn:
-                        Toast.makeText(SettingActivity.this, "저장할게유", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.exit_btn:
-                        Toast.makeText(SettingActivity.this, "취소할게유", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.abc_inc:
-                        inabc++;
-                        abc_num.setText(String.valueOf(inabc));
-                        break;
-                    case R.id.abc_dec:
-                        if (inabc == 1) {  // 음수 구분용   MAX는 필요할까?
-                            Toast.makeText(SettingActivity.this, "1 이하는 입력X", Toast.LENGTH_SHORT).show();
-                        } else {
-                            inabc--;
-                            abc_num.setText(String.valueOf(inabc));
-                        }
-                        abc_num.setText(String.valueOf(inabc));
-                        break;
-                }
-            }
         });
     }
+
+    @Override
+    public void onClick(View view) {
+        String abc_str = (String) abc_num.getText();
+        int inabc = (int) Integer.parseInt(abc_str);
+        switch (view.getId()) {
+            case R.id.set_btn:
+                Toast.makeText(SettingActivity.this, "저장할게유", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit_btn:
+                Toast.makeText(SettingActivity.this, "취소할게유", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.abc_inc:
+                inabc++;
+                abc_num.setText(String.valueOf(inabc));
+                break;
+            case R.id.abc_dec:
+                if (inabc == 1) {  // 음수 구분용   MAX는 필요할까?
+                    Toast.makeText(SettingActivity.this, "1 이하는 입력X", Toast.LENGTH_SHORT).show();
+                } else {
+                    inabc--;
+                    abc_num.setText(String.valueOf(inabc));
+                }
+                abc_num.setText(String.valueOf(inabc));
+                break;
+        }
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {}
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {}
 }

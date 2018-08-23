94f5c2482479b373b176bfd9e392ca1e1e3b2a17
yangjihoon <kin7274@naver.com>
2018-08-23 20:49:22 +0900

    블루투스 토스트만 넣어둠
    블루투스 토스트만 넣어둠

--- App02/app/src/main/java/com/example/administrator/app02/CustomDialog.java
+++ App02/app/src/main/java/com/example/administrator/app02/CustomDialog.java
@@ -8,6 +8,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CustomDialog extends Dialog {
 
@@ -34,12 +35,35 @@ public class CustomDialog extends Dialog {
         textTitle = (TextView) findViewById(R.id.textTitle);
         textTitle.setText(title);
 
-//        Button okButton = (Button) findViewById(R.id.okButton);
-//        Button okButton = (Button) findViewById(R.id.okButton);
-//        Button okButton = (Button) findViewById(R.id.okButton);
+//        블루투스를 킨다!
+        Button btnONOFF = (Button) findViewById(R.id.btnONOFF);
+        btnONOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "ONOFF", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+//        블루투스 장치 검색 허용!
+        Button btnDiscoverable_on_off = (Button) findViewById(R.id.btnDiscoverable_on_off);
+        btnDiscoverable_on_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Discoverable_on_off", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        장치 검색을 한다!
+        Button btnFindUnpairedDevices = (Button) findViewById(R.id.btnFindUnpairedDevices);
+        btnFindUnpairedDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "pairedDevices", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        확인 = 종료 버튼
         Button okButton = (Button) findViewById(R.id.okButton);
-
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

3b1cf02af656521c3bcca7bec98d15c0c07d8a1e
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-08-24 15:16:03 +0900

    불루투스 커스텀다이얼로그 수정
    불루투스 커스텀다이얼로그 수정

--- App02/app/src/main/java/com/example/administrator/app02/CustomDialog.java
+++ App02/app/src/main/java/com/example/administrator/app02/CustomDialog.java
@@ -5,77 +5,72 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.graphics.drawable.IconCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CustomDialog extends Dialog {
+public class CustomDialog extends AppCompatActivity {
     // TODO 블루투스 기능 구현(메인에 한걸 일로..)
 
     private MyDialogListener dialogListener;
     private Context context;
-    private TextView textTitle;
-    private String title;
-    private Context resources;
 
-    public CustomDialog(Context context, String title) {
-        super(context);
+    public CustomDialog(Context context) {
         this.context = context;
-        this.title = title;
     }
 
     public void setDialogListener(MyDialogListener dialogListener) {
         this.dialogListener = dialogListener;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_custom);
+    public void callFunction(final MainActivity main_label) {
+        final Dialog dialog = new Dialog(context);
 
-        textTitle = (TextView) findViewById(R.id.textTitle);
-        textTitle.setText(title);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.setContentView(R.layout.dialog_custom);
+
+        dialog.show();
+
+        final Button btnONOFF = ( Button ) dialog.findViewById(R.id.btnONOFF);
+        final Button btnDiscoverable_on_off = ( Button ) dialog.findViewById(R.id.btnDiscoverable_on_off);
+        final Button btnFindUnpairedDevices = ( Button ) dialog.findViewById(R.id.btnFindUnpairedDevices);
 
         // 블루투스를 킨다!
-        Button btnONOFF = (Button) findViewById(R.id.btnONOFF);
         btnONOFF.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), getResources().getString(R.string.bluetooth1), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Toast.makeText(context, "btnONOFF", Toast.LENGTH_SHORT).show();
             }
         });
-
         // 블루투스 장치 검색 허용!
-        Button btnDiscoverable_on_off = (Button) findViewById(R.id.btnDiscoverable_on_off);
         btnDiscoverable_on_off.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), getResources().getString(R.string.bluetooth2), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Toast.makeText(context, "btnDiscoverable_on_off.", Toast.LENGTH_SHORT).show();
             }
         });
-
         // 장치 검색을 한다!
-        Button btnFindUnpairedDevices = (Button) findViewById(R.id.btnFindUnpairedDevices);
         btnFindUnpairedDevices.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), getResources().getString(R.string.bluetooth3), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Toast.makeText(context, "btnFindUnpairedDevices.", Toast.LENGTH_SHORT).show();
             }
         });
 
         // 확인 = 종료 버튼
-        Button okButton = (Button) findViewById(R.id.okButton);
+        final Button okButton = ( Button ) dialog.findViewById(R.id.okButton);
+
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                cancel();
+            public void onClick(View view) {
+                Toast.makeText(context, "취소 했습니다.", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
             }
         });
-    }
-
-    public Context getResources() {
-        return resources;
     }
 }

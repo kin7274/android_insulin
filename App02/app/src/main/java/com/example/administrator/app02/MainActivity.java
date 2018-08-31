8d09ce9ebbf349bc9555956b622f02e5a326d866
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-08-31 21:00:27 +0900

    Sharedpreference 이용 설정값 저장
    Sharedpreference 이용 설정값 저장

--- App02/app/src/main/java/com/example/administrator/app02/MainActivity.java
+++ App02/app/src/main/java/com/example/administrator/app02/MainActivity.java
@@ -45,8 +45,10 @@ import static com.example.administrator.app02.MyRecyclerAdapter.*;
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewClickListener {
     // TODO 타임라인형식
 
+    // 쉐어드
+    EditText et;
+
     SharedPreferences sharedPreferences;
-    EditText editName;
     private MyRecyclerAdapter mAdapter;
     RecyclerView recyclerView;
     List<CardItem> dataList;
@@ -58,43 +60,14 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewCli
 
     // 설정 페이지에서 저장된 값 표시
     TextView data_view;
-    String sfName = "SettingData";
 
     // 메인
-    @SuppressLint("ResourceType")
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // 설정페이지로부터 저장되있는 값 받음 AA = 설정페이지에서 설정한 값
-//        Intent intent = getIntent();
-//        String AA = intent.getStringExtra("settingData");
         data_view = ( TextView ) findViewById(R.id.data_view);
-//        data_view.setText(AA);
-
-        if(Init == 1) {
-            Toast.makeText(getApplicationContext(), Init +" =1 뻔하죠?", Toast.LENGTH_LONG).show();
-//            ////////////////////////////////////////////////////////////////////////
-//            ////////////////////////////////////////////////////////////////////////
-//            ////////// 상태 불러오자 ////////////////////////////////////////////////
-//            Toast.makeText(getApplicationContext(), "저장값을 불러오겠습니다!", Toast.LENGTH_LONG).show();
-//
-//            SharedPreferences sf = getSharedPreferences(sfName, 0);
-//            String str = sf.getString("name", ""); // 키값으로 꺼냄
-//            data_view.setText(str);
-//
-//            Toast.makeText(getApplicationContext(), "불러오기 성공", Toast.LENGTH_LONG).show();
-//            ////////////////////////////////////////////////////////////////////////
-//            ////////////////////////////////////////////////////////////////////////
-//
-            Init++;
-            Toast.makeText(getApplicationContext(), Init + "=2 뻔하죠?", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(getApplicationContext(), Init + "1이 아니죠?", Toast.LENGTH_LONG).show();
-        }
 
         // 툴바
         Toolbar mytoolbar = ( Toolbar ) findViewById(R.id.my_toolbar);
@@ -102,9 +75,9 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewCli
         getSupportActionBar().setTitle("");
 
         // 상태바 색 변경
-        View view = getWindow().getDecorView();
-        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        getWindow().setStatusBarColor(Color.parseColor(getResources().getString(R.color.colorPrimaryPurle)));
+//        View view = getWindow().getDecorView();
+//        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//        getWindow().setStatusBarColor(Color.parseColor(getResources().getString(R.color.colorPrimaryPurle)));
 
         RecyclerView recyclerView = ( RecyclerView ) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(false);
@@ -128,42 +101,34 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewCli
         // 이쁘면 메뉴얼쪽에도 추가하자
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(), new LinearLayoutManager(this).getOrientation());
         recyclerView.addItemDecoration(dividerItemDecoration);
+
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        String str = pref.getString("PREF_STRNAME", "");
+        data_view.setText(str);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1) {
-            if(resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
                 String result = data.getStringExtra("AA");
-            data_view.setText(result);
+                data_view.setText(result);
             }
         }
-//        super.onActivityResult(requestCode, resultCode, data);
     }
 
-        ////////////////////////////////////
-    ////////////////////////////////////
-    ////////////////////////////////////
-//    @Override
-//    protected void onStop(){
-//        super.onStop();
-//        // Activity 가 종료되기 전에 저장한다
-//        // SharedPreferences 에 설정값(특별히 기억해야할 사용자 값)을 저장하기
-//        Toast.makeText(getApplicationContext(), "급종;;;", Toast.LENGTH_LONG).show();
-//        SharedPreferences sf = getSharedPreferences(sfName, 0);
-//        SharedPreferences.Editor editor = sf.edit();//저장하려면 editor가 필요
-//        String str = data_view.getText().toString(); // 사용자가 입력한 값
-//        editor.putString("name", str); // 입력
-//        editor.putString("xx", "xx"); // 입력
-//        editor.commit(); // 파일에 최종 반영함
-//        Toast.makeText(getApplicationContext(), "저장 성공", Toast.LENGTH_LONG).show();
-//    }
-    ////////////////////////////////////
-    ////////////////////////////////////
-    ////////////////////////////////////
+    // 액티비티 종료 전에 저장!
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        String strrr = data_view.getText().toString();
+        editor.putString("PREF_STRNAME", strrr);
+        editor.commit();
+    }
 
-
-//  클릭 이벤트
+    //  클릭 이벤트
     @Override
     public void onItemClicked(int position) {
         Toast.makeText(getApplicationContext(), position + "번째 삭제", Toast.LENGTH_LONG).show();
@@ -208,22 +173,17 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewCli
                             Toast.makeText(getApplicationContext(), "설정부터하세요.", Toast.LENGTH_SHORT).show();
                         } else {
                             dialog.cancel();
-                                    // 현재시간 미구현
+                            // 현재시간 미구현
                             String data = "현재시간, " + items[selectedItem[0]] + ", " + data_view.getText().toString();
-//
-//
-//
 
                             // 리스트에 데이터를 입력
                             dataList.add(new CardItem("d", "d"));
                             // listview 갱신
                             mAdapter.notifyDataSetChanged();
 //                            dataList.add(new CardItem("제발", "제발요"));
-
 //                            Toast.makeText(getApplicationContext(), items[selectedItem[0]] + ", " + data_view.getText().toString(), Toast.LENGTH_SHORT).show();
 
 //                            Toast.makeText(getApplicationContext(), "입력 완료", Toast.LENGTH_SHORT).show();
-
                             // 데이터가 추가된 위치(리스트뷰의 마지막)으로 포커스를 이동시킨다.
 //                                    recyclerView.setSelection(dataList.size() - 1);
 //                            finish();
@@ -286,46 +246,4 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewCli
         }
         return super.onOptionsItemSelected(item);
     }
-
-    // 데이터 저장용
-    // 앱이 중지될 때 데이터 다시 불러오기용
-//    @Override
-//    protected void onPause(){
-//        super.onPause();
-//        Toast.makeText(this, "onPause 호출됨", Toast.LENGTH_LONG).show();
-//        saveState();
-//    }
-//
-//    // 앱이 다시 시작할 때 데이터 다시 불러오기용
-//    @Override
-//    protected void onResume(){
-//        super.onResume();
-//        Toast.makeText(this, "onResume 호출됨", Toast.LENGTH_LONG).show();
-//        restoreState();
-//    }
-//
-    // 데이터 저장용
-//    protected void saveState(){
-//        SharedPreferences pref = getSharedPreferences("pref", 0);
-//        SharedPreferences.Editor editor = pref.edit();
-//        editor.putString("dataview", data_view.getText().toString());
-//        editor.commit();
-//    }
-//
-//    // 데이터 불러오기용
-//    protected void restoreState(){
-//        SharedPreferences pref = getSharedPreferences("pref", 0);
-//        if((pref != null) && (pref.contains("dataview"))){
-//            String name = pref.getString("dataview", "");
-//            data_view.setText(name);
-//        }
-//    }
-//
-//    // 저장소 클리어용
-//    protected void clearMyPerfs(){
-//        SharedPreferences pref = getSharedPreferences("pref", 0);
-//        SharedPreferences.Editor editor = pref.edit();
-//        editor.clear();
-//        editor.commit();
-//    }
 }

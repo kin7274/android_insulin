cc543eab619074ba56ed2a9e736f92ed6fbe2c9b
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-05 14:22:09 +0900

    메인 뷰 수정
    메인 뷰 수정

--- App02/app/src/main/java/com/example/administrator/app02/MainActivity.java
+++ App02/app/src/main/java/com/example/administrator/app02/MainActivity.java
@@ -54,28 +54,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mContext = this;
         setToolbar();
         set();
-//        recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
-//        recycler_view.setHasFixedSize(false);
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        // 반대로 쌓기
-//        layoutManager.setReverseLayout(true);
-//        layoutManager.setStackFromEnd(true);
-//        recycler_view.setLayoutManager(layoutManager);
-//
-//        try {
-//            settinglists = new ArrayList<>();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        // 어댑터 설정
-//        mAdapter = new SettingRecyclerAdapter(settinglists);
-//        mAdapter.setOnClickListener(this);
-//        recycler_view.setAdapter(mAdapter);
-//
-//        // 구분선
-//        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(), new LinearLayoutManager(this).getOrientation());
-//        recycler_view.addItemDecoration(dividerItemDecoration);
+        recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
+        recycler_view.setHasFixedSize(false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        // 반대로 쌓기
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recycler_view.setLayoutManager(layoutManager);
+
+        try {
+            settinglists = new ArrayList<>();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 어댑터 설정
+        mAdapter = new SettingRecyclerAdapter(settinglists);
+        mAdapter.setOnClickListener(this);
+        recycler_view.setAdapter(mAdapter);
+
+        // 구분선
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(), new LinearLayoutManager(this).getOrientation());
+        recycler_view.addItemDecoration(dividerItemDecoration);
 
         // 저장데이터 불러오기
         SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
@@ -115,6 +115,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    // 툴바
     public void setToolbar(){
         // 툴바
         Toolbar mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
@@ -122,62 +123,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getSupportActionBar().setTitle("");
     }
 
+    // 객체
     public void set() {
-        textview1 = (TextView) findViewById(R.id.textview1);
-        textview2 = (TextView) findViewById(R.id.textview2);
+//        textview1 = (TextView) findViewById(R.id.textview1);
+//        textview2 = (TextView) findViewById(R.id.textview2);
 //        setting_data = (TextView) findViewById(R.id.setting_data);
-        Button btn1 = (Button) findViewById(R.id.btn1);
-        Button btn2 = (Button) findViewById(R.id.btn2);
-        // 설정 끝, 다음 페이지로 이동
-        Button next_btn = (Button) findViewById(R.id.next_btn);
+//        Button btn1 = (Button) findViewById(R.id.btn1);
+//        Button btn2 = (Button) findViewById(R.id.btn2);
 //        Button action_setting = (Button) findViewById(R.id.action_setting);
 //        Button action_setting2 = (Button) findViewById(R.id.action_setting2);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        next_btn.setOnClickListener(this);
+//        btn1.setOnClickListener(this);
+//        btn2.setOnClickListener(this);
 //        action_setting.setOnClickListener(this);
 //        action_setting2.setOnClickListener(this);
-        //
+
+        // 약품1
         TextView setting_kind_1 = (TextView) findViewById(R.id.setting_kind_1);
         TextView setting_name_1 = (TextView) findViewById(R.id.setting_name_1);
         TextView setting_unit_1 = (TextView) findViewById(R.id.setting_unit_1);
         TextView setting_status_1 = (TextView) findViewById(R.id.setting_status_1);
-        TextView setting_kind_2 = (TextView) findViewById(R.id.setting_kind_1);
-        TextView setting_name_2 = (TextView) findViewById(R.id.setting_name_1);
-        TextView setting_unit_2 = (TextView) findViewById(R.id.setting_unit_1);
-        TextView setting_status_2 = (TextView) findViewById(R.id.setting_status_1);
-        Button setting_set = (Button) findViewById(R.id.setting_set);
         setting_kind_1.setOnClickListener(this);
         setting_name_1.setOnClickListener(this);
         setting_unit_1.setOnClickListener(this);
         setting_status_1.setOnClickListener(this);
+        // 약품2
+        TextView setting_kind_2 = (TextView) findViewById(R.id.setting_kind_1);
+        TextView setting_name_2 = (TextView) findViewById(R.id.setting_name_1);
+        TextView setting_unit_2 = (TextView) findViewById(R.id.setting_unit_1);
+        TextView setting_status_2 = (TextView) findViewById(R.id.setting_status_1);
         setting_kind_2.setOnClickListener(this);
         setting_name_2.setOnClickListener(this);
         setting_unit_2.setOnClickListener(this);
         setting_status_2.setOnClickListener(this);
+        // 약품 저장
+        Button setting_set = (Button) findViewById(R.id.setting_set);
         setting_set.setOnClickListener(this);
+        // 장치 스캔 버튼
+        Button scan_device = (Button) findViewById(R.id.scan_device);
+        scan_device.setOnClickListener(this);
+        // 페어링 버튼
+        Button pairing_btn = (Button) findViewById(R.id.pairing_btn);
+        pairing_btn.setOnClickListener(this);
+        // 설정 끝, 다음 페이지로 이동
+        Button next_btn = (Button) findViewById(R.id.next_btn);
+        next_btn.setOnClickListener(this);
     }
 
-    // 설정페이지 인텐트로부터 리시브
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1) {
-            if (resultCode == Activity.RESULT_OK) {
-                setting_insulin1 = data.getStringExtra("AA");
-                settinglists.add(new CardItem_Setting(setting_insulin1));
-                mAdapter.notifyDataSetChanged();
-                Global.setData1(setting_insulin1);
-            }
-        }
-        if (requestCode == 2) {
-            setting_insulin2 = data.getStringExtra("BB");
-            settinglists.add(new CardItem_Setting(setting_insulin2));
-            mAdapter.notifyDataSetChanged();
-            Global.setData2(setting_insulin2);
-        }
-    }
-
-    // 메뉴.xml
+    // 툴바 메뉴
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -233,12 +225,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent1);
                 break;
             // 페어링
-            case R.id.btn2:
-                final Intent intent2 = new Intent(MainActivity.this, DeviceControlActivity.class);
-                intent2.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, textview1.getText());
-                intent2.putExtra(EXTRAS_DEVICE_ADDRESS, textview2.getText());
-                startActivity(intent2);
-                break;
+//            case R.id.btn2:
+//                final Intent intent2 = new Intent(MainActivity.this, DeviceControlActivity.class);
+//                intent2.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, textview1.getText());
+//                intent2.putExtra(EXTRAS_DEVICE_ADDRESS, textview2.getText());
+//                startActivity(intent2);
+//                break;
             // 설정 끝, 다음으로 이동
 //            case R.id.btn4:
 //                Intent intent4 = new Intent(MainActivity.this, Timeline.class);
@@ -263,8 +255,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "11111 설정 후 내가 돌아왔어");
-        textview1.setText(BluetoothLog.getName());
-        textview2.setText(BluetoothLog.getAddress());
+//        textview1.setText(BluetoothLog.getName());
+//        textview2.setText(BluetoothLog.getAddress());
     }
 
     // 액티비티 종료 전에 저장!

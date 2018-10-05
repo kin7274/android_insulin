9f838ca98203f05392eb2a406125c16a4df74bbc
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-10-05 12:23:56 +0900

    메뉴 수동버튼(+) 삭제
    메뉴 수동버튼(+) 삭제

--- App02/app/src/main/java/com/example/administrator/app02/Intro.java
+++ App02/app/src/main/java/com/example/administrator/app02/Intro.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 
